994e9da1d8d309bc3ad3c207ddc489bdac036960
Thiago Delgado Pinto <thiago-dp@bol.com.br>
2018-03-27 00:54:30 -0300

    Changes main file

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
 /**
  * Performs a 1-wise (aka 1-way) interaction with the items of the given parameters.
  *
- * @see https://en.wikipedia.org/wiki/All-pairs_testing
- *
- * @example
- * ```js
- * console.log(
- *   oneWise( {
- *     "foo": [ "x", "y" ],
- *     "bar": [ "a", "b", "c", "d" ],
- *     "baz": [ "f", "g" ]
- *   } )
- * );
- * // will print:
- * // [
- * //   { "foo": "x", "bar": "a", "baz": "f" ],
- * //   { "foo": "y", "bar": "b", "baz": "g" ],
- * //   { "foo": "x", "bar": "c", "baz": "f" ],  // "foo" and "baz" values may vary (random)
- * //   { "foo": "y", "bar": "d", "baz": "g" ]   // "foo" and "baz" values may vary (random)
- * // ]
- * ```
- *
  * @param {object} map Maps string => array of values, e.g. `{ "foo": [ "x", "y" ], "bar": [ "a", "b", "c" ] }`.
  * @param {function} randomFn Function to generate a pseudo-random number >= 0 and < 1. Assumes `Math.randon()` if not informed.
  * @returns {array} Array with maps with keys and their values.
